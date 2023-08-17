e15ec583138715cdc11399e107b53184f7de8393
Bingo <Bingo0211@163.com>
2023-08-17 20:02:47 +0800

    Changes

---
+++ Python新特性/03_字符串新增方法.py
@@ -0,0 +1,8 @@
+#删除前缀
+result = 'helloworld'
+print(result.removeprefix('hello'))
+print(result.removeprefix('abc'))
+
+#删除后缀
+print(result.removesuffix('world'))
+print(result.removesuffix('World'))
